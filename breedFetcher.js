26c5c88e0592c4e6ec233f437114c896a4a1a6a6
JRam2022 <jasonram778@gmail.com>
2022-04-14 19:00:32 -0700

    breedFetcher exercise

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,20 @@
-`use strict`;
-
-const argv = process.argv.slice(2);
-
 const request = require('request');
-//stores URL because it should never change
-const searchURL = 'https://api.thecatapi.com/v1/breeds/search?q=';
-//Keeps URL and Cat breed search
-const catBreed = searchURL + argv[0];
 
+const fetchBreedDescription = function(breedName, callback) {
 
-request(catBreed, (error, response, body) => {
-  const data = JSON.parse(body);
-  //if errors returns error
-  if (error) {
-    console.log(JSON.stringify(error));
-    return;
-  }
-  //if not a vaild url or cat breed logs message
-  if (data[0] === undefined) {
-    console.log("Must be a valid URL and cat breed");
-    return;
-  }
-  //prints cat breed description
-  console.log(data[0]['description']);
-  
-});
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+    // returns error if undefined or invalid breed
+    if (data[0] === undefined) {
+      let error = "Must be a valid URL and cat breed";
+      return callback(error, null);
+    }
+    
+    //prints cat breed description
+    let desc = data[0]['description'];
+    callback(null, desc);
+    
+  });
+};
+
+module.exports = { fetchBreedDescription };
