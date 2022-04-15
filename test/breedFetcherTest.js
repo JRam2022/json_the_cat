c0db2e83c5d11d17d027a3c5af3ef3152f973c41
JRam2022 <jasonram778@gmail.com>
2022-04-14 19:04:05 -0700

    Test for breedFetcher

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,32 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (error, desc) => {
+      // we expect no error for this scenario
+      assert.equal(error, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+  
+  it('returns a error for a invalid breed, via callback', (done) => {
+    fetchBreedDescription('Apple', (error, desc) => {
+      // we expect error for this scenario
+      
+      const expectedError = "Must be a valid URL and cat breed";
+
+      // compare returned error
+      assert.equal(expectedError, error);
+
+      done();
+    
+    });
+  });
+});
